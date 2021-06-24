e80d2ca14c42344f1e072609816aab467cd6bfab
qwantanius <borshbrawl@gmail.com>
2021-06-25 05:43:58 +0800

    add stealth mode

---
+++ smoky.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+// BASE CONSTANTS
+const string MUTE_OUTPUT = "> /dev/null 2>&1";
+
+bool run(string cmd,bool stealthMode = true){
+	try {
+		string resCommand;
+		resCommand = cmd + MUTE_OUTPUT;
+		if(!stealthMode){
+			resCommand = cmd;
+		}
+		system((resCommand).c_str());
+		return true;
+	} catch (...){
+		return false;
+	}
+}
+
+
+int main(){
+	run("echo stealth mode on");
+	run("echo stealth mode off",false);
+}
